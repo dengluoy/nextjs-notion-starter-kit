a19a202b01ceff019f83c6c23c7c17d2ac145bbc
David <davidhello6@gmail.com>
2022-03-12 12:51:49 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
-
+  rootNotionPageId: 'ccda9c48329b4fd6902f21203ddfedef',
+// https://azure-toucan-6ad.notion.site/ccda9c48329b4fd6902f21203ddfedef
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'David 说',
+  domain: 'https://azure-toucan-6ad.notion.site/ccda9c48329b4fd6902f21203ddfedef',
+  author: 'Dww',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -18,7 +18,7 @@ module.exports = {
 
   // social usernames (optional)
   twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  github: 'https://github.com/dengluoy',
   linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
